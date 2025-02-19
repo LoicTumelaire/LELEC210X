2ea8ae7bee8e7d7fc11aa2abd2f35d9a68f443f3
Loïc <loic.tumelaire@gmail.com>
2025-02-19 13:47:14 +0100

    V2

--- classification/src/classification/__main__.py
+++ classification/src/classification/__main__.py
@@ -1,10 +1,9 @@
-import pickle
-from keras import models
+from keras import models #import pickle
 from pathlib import Path
 from typing import Optional
 
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow, show #import matplotlib.pyplot as plt
 
 import click
 
@@ -15,8 +14,8 @@ from common.logging import logger
 
 from .utils import payload_to_melvecs
 
-import requests
-import json
+from requests import post
+from json import loads
 
 pathFile = Path(__file__).resolve()
 model_dir = str(pathFile)[:-11]+"/../../data/models/"
@@ -24,12 +23,33 @@ mel_dir = str(pathFile)[:-11]+"/../../data/melspecs/"
 
 load_dotenv()
 
+###################################
+# Variables globales
+###################################
 send = True
+save = False
+DEBUG = False
+
+# Adresses + keys
 #hostname = "http://localhost:5000"
 hostname = "http://lelec210x.sipr.ucl.ac.be" # Contest: http://lelec210x.sipr.ucl.ac.be/lelec210x/leaderboard
 #key = "dhdCGK4Xq7EKm-U9Ji1MAHYvPyWBqoimYAU4pknY"
 key = "EPHNDFX0Y_aie6lb6trPdTrw_ob8Gc8yNzIpusWF" # Contest
 
+model = models.load_model(model_dir + "two.keras")
+classes = ["birds", "chainsaw", "fire", "handsaw", "helicopter"]
+history = np.zeros((5,5)) # History of max len of 5 y_pred = [0.1, 0.2, 0.3, 0.4, 0.5], if we add more, the first one is removed
+
+def exponentialWeight(number, expo=0.1):
+    """
+    Calculate the exponential weight of a given number.
+    Parameters:
+        number (float): The input number to be weighted.
+        expo (float, optional): The exponent value to be used in the calculation. Default is 0.1.
+    Returns:
+        float: The exponential weight of the input number.
+    """
+    return np.exp(-expo*number)
 
 @click.command()
 @click.option(
@@ -68,7 +88,6 @@ def main(
     This way, you will directly receive the authentified packets from STDIN
     (standard input, i.e., the terminal).
     """
-    model = models.load_model(model_dir + "two.keras")
     for payload in _input:
         if PRINT_PREFIX in payload:
             payload = payload[len(PRINT_PREFIX) :]
@@ -76,39 +95,52 @@ def main(
             melvecs = payload_to_melvecs(payload, melvec_length, n_melvecs)
             logger.info(f"Parsed payload into Mel vectors: {melvecs}")
 
-            classify = True
-            # If the melvecs has too much noise, we don't classify it, TODO: add in the MCU
+            # If the melvecs has too much noise, we don't classify it, 
+            # TODO: add in the MCU: don't send it
+            noise = False
             if np.max(melvecs) < 1e-3:
-                classify = False
+                noise = True
 
-            if classify:
+            if not noise:
                 # Perform classification
 
-                # Flip the melvecs for the frequencies and reshape it for the model
-                #melvecs = np.flipud(melvecs)
-                melvecs = melvecs[None,...]
+                melvecs = melvecs[None,...] # Add a dimension to the melvecs for the model
 
-                # Plot the melvecs !! Plot à l'envers
-                plt.imshow(melvecs[0,:], origin='lower')
-                plt.show()
+                if DEBUG:
+                    # Plot the melvecs !! Plot à l'envers
+                    imshow(melvecs[0,:], origin='lower')
+                    show()
 
                 # Save the melvecs into a file
-                filename = mel_dir + payload[:10] + ".npy"
-                #np.save(filename, melvecs)
-                #print(melvecs)
-                y_pred = model.predict(melvecs) # [[0.1, 0.2, 0.3, 0.4, 0.5]]
+                if save:
+                    filename = mel_dir + payload[:10] + ".npy"
+                    np.save(filename, melvecs)
 
-                classes = ["birds", "chainsaw", "fire", "handsaw", "helicopter"]
+                # Predict the class probabilities
+                y_pred = model.predict(melvecs) # [0.1, 0.2, 0.3, 0.4, 0.5]
 
-                guess = classes[int(np.argmax(y_pred))]
+                # Add the prediction to the history and delete the last one
+                history = np.roll(history, 1, axis=0)
+                history[0] = y_pred
+                
+                # Smooth the prediction
+                smoothed_pred = np.zeros(5)
+                for i in range(5):
+                    smoothed_pred += history[i]*exponentialWeight(i)
+                smoothed_pred /= np.sum(smoothed_pred)
 
-                print(f"Prediction: {guess}")
-                print(f"Probabilities: {y_pred}")
+                # Get the most probable class
+                guess = classes[int(np.argmax(smoothed_pred))]
+
+                if DEBUG:
+                    print(f"Probabilities: {y_pred}")
+                    print(f"Prediction: {guess}")
 
                 # Send to the server if the probabilities are high enough
                 if send and np.max(y_pred) > 0.5:
-                    response = requests.post(f"{hostname}/lelec210x/leaderboard/submit/{key}/{guess}", timeout=1)
+                    print("Sending to the server")
+                    response = post(f"{hostname}/lelec210x/leaderboard/submit/{key}/{guess}", timeout=1)
                     # All responses are JSON dictionaries
-                    response_as_dict = json.loads(response.text)
+                    response_as_dict = loads(response.text)
                     print(response_as_dict)
 
