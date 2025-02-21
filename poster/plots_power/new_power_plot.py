ac88f768965a348461909714307dd043fa8067c1
bemang <antonuttiadrien@gmail.com>
2025-02-21 15:47:14 +0100

    power plot with o3 (not terrible)

---
+++ poster/plots_power/new_power_plot.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+_, time, volt = np.loadtxt('o3_compilation_optimisation.csv', delimiter=',', unpack=True, skiprows=8)
+_, time2, volt2 = np.loadtxt('scopy mesures_mcu.csv', delimiter=',', unpack=True, skiprows=8)
+
+#keep only time between 4.95 and 6.195
+volt = volt[(time >= 4.95) & (time <= 6.195)]
+time = time[(time >= 4.95) & (time <= 6.195)]
+
+#keep between 6.616 and 7.867
+volt2 = volt2[(time2 >= 6.616) & (time2 <= 7.867)]
+time2 = time2[(time2 >= 6.616) & (time2 <= 7.867)]
+
+# start time at 0
+time = time - time[0]
+time2 = time2 - time2[0]
+
+R = 27
+Vdd = 3.3
+
+power = volt/R*Vdd*1000
+power2 = volt2/R*Vdd*1000
+
+plt.plot(time, power, color='blue', alpha=0.8, label="Normal compilation")
+plt.plot(time2, power2, color='red', alpha=0.8, label="O3 compilation")
+plt.xlabel('Time (s)')
+plt.ylabel('Power (mW)')
+plt.legend()
+plt.show()
+#plt.savefig('o3_power.pdf', bbox_inches='tight')
